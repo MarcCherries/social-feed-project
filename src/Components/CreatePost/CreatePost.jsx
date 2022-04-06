14d6959533bcd241611f65745d9e570e10317c2f
Chris Creamer <chrisc1983@gmail.com>
2022-04-06 09:22:49 -0700

    changed the like icons to solid icons so I can fill them instead of outline and added a check to see if no input was submitted so I can alert user to try again

--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -3,27 +3,39 @@ import "./CreatePost.css"
 
 const CreatePost = (props) => {
     
-    const [user, setUser] = useState ('Type your name here....')
-    const [comment, setComment] = useState ('Type your comment here...')
+    const [user, setUser] = useState ('Double-click to type your name...')
+    const [comment, setComment] = useState ('Double-click to type your comment...')
 
     function handleSubmit(event){
         event.preventDefault();
+        let clearUserField= () => { setUser('Double-click to type your name...')};
+        let clearCommentField= () => {setComment('Double-click to type your comment...')};
         let newPost = {
             user: user,
             comment: comment,
         }
-        props.addPost(newPost);   
+        if (!user || user==="Double-click to type your name..." || !comment || comment==="Double-click to type your name..."){
+            alert("No input detected! Please try again!");
+        }
+    
+        else {
+        props.addPost(newPost);
+        clearUserField();
+        clearCommentField();
+        }
+        
+
     }
     return (   
         <div>
             <form className="create-box" onSubmit={handleSubmit}>
                 <div className="name-input">
                 <label className="name-label">Name</label>
-                <input className="input-field"type="text" size="41" onClick={() => setUser("")} onChange={(event) => setUser(event.target.value)} value={user} ></input>
+                <input className="input-field"type="text" size="41" onDoubleClick={() => setUser("")} onChange={(event) => setUser(event.target.value)} value={user} ></input>
                 </div>
                 <div className="comment-container">
                 <label className="label-text">Comment</label>
-                <textarea type="text" className="comment-box2"  onClick={() => setComment("")} onChange={(event) => setComment(event.target.value)} value={comment} ></textarea>
+                <textarea type="text" className="comment-box2"  onDoubleClick={() => setComment("")} onChange={(event) => setComment(event.target.value)} value={comment} ></textarea>
                 <button type="submit" class="button-one btn btn-info" >Post</button>
                 </div>
             </form>
